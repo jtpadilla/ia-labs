8c41287a600e669facfef774c820486b4be7654f
padilla <padilla@uji.es>
2026-02-26 16:51:51 +0100

    Ejamplos javelit

---
+++ java/example/javelit/005-drawlinechart/src/main/java/io/github/jtpadilla/example/javelit/drawlinechart/DrawLineChart.java
@@ -0,0 +1,25 @@
+package io.github.jtpadilla.example.javelit.drawlinechart;
+
+import io.javelit.core.Jt;
+import io.javelit.core.Server;
+import org.icepear.echarts.Line;
+
+class DrawLineChart {
+
+    static void main(String[] args) {
+        Server.builder(DrawLineChart::new, 8080)
+                .build()
+                .start();
+
+    }
+
+    public DrawLineChart() {
+        Line line = new Line()
+                .addXAxis(new String[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" })
+                .addYAxis()
+                .addSeries(new Number[] { 150, 230, 224, 218, 135, 147, 260 });
+
+        Jt.echarts(line).use();
+    }
+
+}
