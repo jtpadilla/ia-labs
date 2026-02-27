0bafcb5f25d81f3f9d430f05b6aafdfed7bdf5c8
padilla <padilla@uji.es>
2026-02-27 20:16:41 +0100

    Ejamplos javelit

--- java/example/ai/interactions/demo/researchfrontend/src/main/java/io/github/jtpadilla/example/interactions/demo/researchfrontend/impl/UserInterface.java
+++ java/example/ai/interactions/demo/researchfrontend/src/main/java/io/github/jtpadilla/example/interactions/demo/researchfrontend/impl/UserInterface.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class UserInterface implements JtRunnable {
 
