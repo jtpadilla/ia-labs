c4e4ef78f1beb5475b73c63a03cbbf5dad44997e
padilla <padilla@uji.es>
2026-03-01 21:10:13 +0100

    a2a

---
+++ java/platform/ianews/vanilla/dispatcher/src/main/java/io/github/jtpadilla/ianews/vanilla/service/dispatcher/VanillaIANewsDispatcherService.java
@@ -0,0 +1,8 @@
+package io.github.jtpadilla.ianews.vanilla.service.dispatcher;
+
+import io.github.jtpadilla.a2a.service.dispatcher.DispatcherService;
+
+public class VanillaIANewsDispatcherService implements DispatcherService {
+
+}
+
--- java/platform/laliga/vanilla/dispatcher/src/main/java/io/github/jtpadilla/laliga/vanilla/service/dispatcher/VanillaLaligaDispatcherService.java
+++ java/platform/laliga/vanilla/dispatcher/src/main/java/io/github/jtpadilla/laliga/vanilla/service/dispatcher/VanillaLaligaDispatcherService.java
@@ -2,7 +2,7 @@ package io.github.jtpadilla.a2a.vanilla.service.dispatcher;
 
 import io.github.jtpadilla.a2a.service.dispatcher.DispatcherService;
 
-public class VanillaDispatcherService implements DispatcherService {
+public class VanillaLaligaDispatcherService implements DispatcherService {
 
 }
 
