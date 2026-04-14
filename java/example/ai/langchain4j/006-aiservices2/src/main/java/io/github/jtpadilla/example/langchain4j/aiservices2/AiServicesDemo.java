2839ba164e464ce1fa73dfb85f4429837c5c9815
padilla <padilla@speedycontrol.com>
2026-04-14 13:13:11 +0200

    langchain4j examples.

---
+++ java/example/ai/langchain4j/006-aiservices2/src/main/java/io/github/jtpadilla/example/langchain4j/aiservices2/AiServicesDemo.java
@@ -0,0 +1,34 @@
+package io.github.jtpadilla.example.langchain4j.aiservices1;
+
+import dev.langchain4j.service.AiServices;
+import io.helidon.config.Config;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+
+public class AiServicesDemo {
+
+    final static private String MODEL = "gemini-3.1-flash-lite-preview";
+
+    final static private String API_KEY = Config.global().get("gemini-api-key").asString().orElseThrow(
+            () -> new IllegalStateException("Configuration key 'gemini-api-key' is required"));
+
+    public static void main(String[] args) {
+
+        ChatModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(API_KEY)
+                .modelName(MODEL)
+                .logRequestsAndResponses(true) // Útil para debug en Bazel
+                .build();
+
+        interface Assistant {
+           String chat(String userMessage);
+        }
+
+        Assistant assistant = AiServices.create(Assistant.class, model);
+
+        String answer = assistant.chat("Hello");
+        System.out.println(answer); // Hello, how can I help you?
+
+    }
+
+}
