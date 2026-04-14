ccecceff499337da7eba16a2664b11b184c65e57
padilla <padilla@speedycontrol.com>
2026-04-14 19:08:26 +0200

    langchain4j examples.

--- java/example/ai/langchain4j/017-squentialworkflow/src/main/java/io/github/jtpadilla/example/langchain4j/sequentialworkflow/AgentDemo.java
+++ java/example/ai/langchain4j/017-squentialworkflow/src/main/java/io/github/jtpadilla/example/langchain4j/sequentialworkflow/AgentDemo.java
@@ -18,6 +18,41 @@ public class AgentDemo {
     final static private String API_KEY = Config.global().get("gemini-api-key").asString().orElseThrow(
             () -> new IllegalStateException("Configuration key 'gemini-api-key' is required"));
 
+    interface CreativeWriter {
+        @UserMessage("""
+            You are a creative writer.
+            Generate a draft of a story no more than
+            3 sentences long around the given topic.
+            Return only the story and nothing else.
+            The topic is {{topic}}.
+            """)
+        @Agent("Generates a story based on the given topic")
+        String generateStory(@V("topic") String topic);
+    }
+
+    interface AudienceEditor {
+        @UserMessage("""
+            You are a professional editor.
+            Analyze and rewrite the following story to better align
+            with the target audience of {{audience}}.
+            Return only the story and nothing else.
+            The story is "{{story}}".
+            """)
+        @Agent("Edits a story to better fit a given audience")
+        String editStory(@V("story") String story, @V("audience") String audience);
+    }
+
+    interface StyleEditor {
+        @UserMessage("""
+            You are a professional editor.
+            Analyze and rewrite the following story to better fit and be more coherent with the {{style}} style.
+            Return only the story and nothing else.
+            The story is "{{story}}".
+            """)
+        @Agent("Edits a story to better fit a given style")
+        String editStory(@V("story") String story, @V("style") String style);
+    }
+
     public static void main(String[] args) {
 
         ChatModel model = GoogleAiGeminiChatModel.builder()
@@ -25,41 +60,6 @@ public class AgentDemo {
                 .modelName(MODEL)
                 .logRequestsAndResponses(true)
                 .build();
-
-        interface CreativeWriter {
-            @UserMessage("""
-            You are a creative writer.
-            Generate a draft of a story no more than
-            3 sentences long around the given topic.
-            Return only the story and nothing else.
-            The topic is {{topic}}.
-            """)
-            @Agent("Generates a story based on the given topic")
-            String generateStory(@V("topic") String topic);
-        }
-
-        interface AudienceEditor {
-            @UserMessage("""
-            You are a professional editor.
-            Analyze and rewrite the following story to better align
-            with the target audience of {{audience}}.
-            Return only the story and nothing else.
-            The story is "{{story}}".
-            """)
-            @Agent("Edits a story to better fit a given audience")
-            String editStory(@V("story") String story, @V("audience") String audience);
-        }
-
-        interface StyleEditor {
-            @UserMessage("""
-            You are a professional editor.
-            Analyze and rewrite the following story to better fit and be more coherent with the {{style}} style.
-            Return only the story and nothing else.
-            The story is "{{story}}".
-            """)
-            @Agent("Edits a story to better fit a given style")
-            String editStory(@V("story") String story, @V("style") String style);
-        }
 
         CreativeWriter creativeWriter = AgenticServices
                 .agentBuilder(CreativeWriter.class)
